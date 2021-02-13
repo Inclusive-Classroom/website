988b831688aea7f1dd18f32d278f10fae3613cc3
dhanbury <dhanb001@odu.edu>
2021-02-13 00:03:06 -0500

    Add list of names to bios.php.

--- bios.php
+++ bios.php
@@ -7,6 +7,15 @@ include_once('header.php');
 <? # Add custom HTML content below this line ?>
 <h1>Biographies</h1>
 
+<ul>
+    <li>Dalton Hanbury</li>
+    <li>Grant Ralls</li>
+    <li>Randy Lane</li>
+    <li>Colton Hurst</li>
+    <li>Greg Hubbard</li>
+    <li>Travis Bennett</li>
+</ul>
+
 <? # Do not change things below this line ?>
 <?
 include_once('footer.php')
