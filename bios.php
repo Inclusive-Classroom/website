d304a32a269f5607a535b4f2cb39240db5262d1b
dhanbury <dhanb001@odu.edu>
2021-02-14 22:00:45 -0500

    Add Bio descriptions and images for Randy and Dalton in bios.php

--- bios.php
+++ bios.php
@@ -8,12 +8,23 @@ include_once('header.php');
 <h1>Biographies</h1>
 
 <ul>
-    <li>Colton Hurst</li>
-    <li>Dalton Hanbury</li>
-    <li>Grant Ralls</li>
-    <li>Greg Hubbard</li>
-    <li>Randy Layne</li>
-    <li>Travis Bennett</li>
+<li><h3>Colton Hurst</h3></li>
+    <li><img src="https://cdn.discordapp.com/attachments/808482595339894794/810702556606496768/IMG-4434_square.jpg" alt="Dalton_Img" style="width:200px;height:200px;">
+    <h3>Dalton Hanbury</h3>
+    Dalton Hanbury is a Third year student at ODU studying
+     for a Bachelor's in Computer Science. He's currently working part time for 
+     Nauticus in downtown Norfolk as a Museum Educator. When he's not doing 
+     classwork or working at Nauticus, he's usually playing airsoft with his team
+      or building models.</li>
+    <li><h3>Grant Ralls</h3></li>
+    <li><h3>Greg Hubbard</h3></li>
+    <li><img src="https://cdn.discordapp.com/attachments/808482595339894794/810583547353366558/Treehouse_FSJS_Profile_crop.jpg" alt="Randy_Img" style="width:200px;height:200px;">
+    <h3>Randy Layne</h3>
+    Randy Layne is a linked Bachelor/Master Computer Science
+     student at ODU and a full time Ruby on Rails developer and Snowflake SQL
+      data engineer for Treehouse. Outside of studying and working he enjoys
+       being a single dad of a wonderful 12 year young lady.</li>
+    <li><h3>Travis Bennett</h3></li>
 </ul>
 
 <? # Do not change things below this line ?>
