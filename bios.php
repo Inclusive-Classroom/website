e7f389da480102e9b3e798bad698b8602829728f
rlayne <rlayn001@odu.edu>
2021-02-15 10:48:04 -0500

    Fixed grammar in Randy's profile

--- bios.php
+++ bios.php
@@ -36,7 +36,7 @@ include_once('header.php');
     Randy Layne is a linked Bachelor/Master Computer Science
     student at ODU and a full time Ruby on Rails developer and Snowflake SQL 
     data engineer for Treehouse. Outside of studying and working he enjoys 
-    being a single dad of a wonderful 12 year young lady.</li>
+    being a single dad of a wonderful 12 year old young lady.</li>
     <li><img src="https://cdn.discordapp.com/attachments/808482595339894794/810890734198587432/travis.jpg" alt="Travis_Img" style="width:200px;height:200px;">
     <h3>Travis Bennett</h3>
     Travis Bennett is a computer science senior at ODU. In his free time he 
