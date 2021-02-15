e5b104557155cff649f61e10da23ece13c7456e4
dhanbury <dhanb001@odu.edu>
2021-02-14 23:04:52 -0500

    Add Greg's bio info to bios.php

--- bios.php
+++ bios.php
@@ -18,7 +18,11 @@ include_once('header.php');
       or building models.</li>
     <li><h3>Grant Ralls</h3></li>
     <li><img src="https://cdn.discordapp.com/attachments/808482595339894794/810720680604991538/GregPFP.png" alt="Greg_Img" style="width:200px;height:200px;">
-    <h3>Greg Hubbard</h3></li>
+    <h3>Greg Hubbard</h3>Gregory Hubbard is a 4th year computer science student
+     with a passion for learning. Currently, he works as an undergraduate research 
+     assistant in developing computer vision based behavioral imaging for 
+      clinical measures of autism spectrum disorder.  His future goals include 
+      bridging the diverse applications of computer science to medicine.</li>
     <li><img src="https://cdn.discordapp.com/attachments/808482595339894794/810583547353366558/Treehouse_FSJS_Profile_crop.jpg" alt="Randy_Img" style="width:200px;height:200px;">
     <h3>Randy Layne</h3>
     Randy Layne is a linked Bachelor/Master Computer Science
