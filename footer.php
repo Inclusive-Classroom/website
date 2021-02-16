60f9d2f6903e0d2e51da7cc12dc360e36a61b28d
rlayne <rlayn001@odu.edu>
2021-02-16 18:52:17 -0500

    Add styled footer

--- footer.php
+++ footer.php
@@ -1,5 +1,20 @@
 </main>
 
+<footer class="navbar navbar-dark bg-dark fixed-bottom">
+    <ul class="navbar-nav text-white-50" >
+        <li class="nav-item">&copy; <?= date("Y") ?> Team Gold - Spring 2021</li>
+    </ul>
+    <ul class="nav justify-content-end flex-row" >
+        <li class="nav-item">
+            <a href="https://www.odu.edu" target="_blank" class="nav-link text-white-50" >Old Dominion University</a>
+        </li>
+        <li class="nav-item" >
+            <a href="https://www.cs.odu.edu" target="_blank" class="nav-link text-white-50" >ODU Department of Computer Science</a>
+        </li>
+    </ul>
+</footer>
+
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js"></script>
