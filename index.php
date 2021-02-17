26475d048ade38babec912f3af021f75952aac62
rlayne <rlayn001@odu.edu>
2021-02-16 20:37:03 -0500

    Fix trademark spacing

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once('header.php');
 <? # Add custom HTML content below this line ?>
 <div class="container">
     <div class="jumbotron bg-transparent-25">
-        <h1 class="display-4">Inclusive Classroom &trade;</h1>
+        <h1 class="display-4">Inclusive Classroom&trade;</h1>
         <p class="lead">Helping solve the problem of underprivileged students being adversely affected by having to
             learn online because of a lack of stable high-speed Internet.</p>
         <hr class="my-4">
