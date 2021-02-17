56459df23a4a5b0ed263ed8aa5915ffb5aa34022
rlayne <rlayn001@odu.edu>
2021-02-16 20:06:00 -0500

    Add hero to home page

--- index.php
+++ index.php
@@ -6,7 +6,15 @@ include_once('header.php');
 
 <? # Add custom HTML content below this line ?>
 <div class="container">
-    <h1>Home</h1>
+    <div class="jumbotron bg-transparent-25">
+        <h1 class="display-4">Inclusive Classroom &trade;</h1>
+        <p class="lead">Helping solve the problem of underprivileged students being adversely affected by having to
+            learn online because of a lack of stable high-speed Internet.</p>
+        <hr class="my-4">
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="presentations.php" role="button">Learn more</a>
+        </p>
+    </div>
 </div>
 <? # Do not change things below this line ?>
 <?
