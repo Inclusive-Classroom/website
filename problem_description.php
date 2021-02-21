0765c1fe3dea255f0125a5fc5daa0264b9d491ff
Grant Ralls <grall001@odu.edu>
2021-02-20 21:07:12 -0500

    adding problem characteristics.

--- problem_description.php
+++ problem_description.php
@@ -10,6 +10,31 @@ include_once('header.php');
     <h1>Societal Problem Identification</h1>
     <p>Young underprivileged students are adversely affected by having to learn online because of a lack of stable high-speed Internet(Anderson) and because they lack the knowledge and support at home needed to adapt when the Internet fails.</p>
   </div>
+  <div>
+    <h1>Problem Characteristics</h1>
+      <ul>
+        <li>
+          <h2>Affects younger students most</h2>
+          <p>They have not built-up study habits. Older students are more familiar with apps.</p>
+        </li>
+        <li>
+          <h2>Lack of technical knowledge at home</h2>
+          <p>Parents may not be "tech savvy". Parents may also be at work.</p>
+        </li>
+        <li>
+          <h2>Difficult to reach teachers</h2>
+          <p>Limited forms of contact. IT is not the teachers specialty.</p>
+        </li>
+        <li>
+          <h2>Lack of consistent high-speed internet at home</h2>
+          <p>Public internet is potentionally dangerous and slow. These public wifi providers may also have time limits for when a user can be connected.</p>
+        </li>
+        <li>
+          <h2>COVID is not going away</h2>
+          <p>On 1/4/2021 we had the highest number of cases in the VA. The curve is continuing to trend upwards.</p>
+        </li>
+      </ul>
+  </div>
 </div>
 <? # Do not change things below this line ?>
 <?php
