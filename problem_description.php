7ee08e15a2e2941c5f4c902fd172e3bc7a15ae1b
Grant Ralls <grall001@odu.edu>
2021-02-20 20:54:11 -0500

    adding problem description php file

---
+++ problem_description.php
@@ -0,0 +1,14 @@
+<?php
+$PageTitle="Home";
+
+include_once('header.php');
+?>
+
+<? # Add custom HTML content below this line ?>
+<div class="container">
+  <h1>Content</h1>
+</div>
+<? # Do not change things below this line ?>
+<?php
+include_once('footer.php')
+?>
