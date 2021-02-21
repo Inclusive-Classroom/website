fed68426c51b2ee5c97601eb9cc3017636dc6904
Grant Ralls <grall001@odu.edu>
2021-02-20 21:01:03 -0500

    adding societal problem identification

--- problem_description.php
+++ problem_description.php
@@ -6,7 +6,10 @@ include_once('header.php');
 
 <? # Add custom HTML content below this line ?>
 <div class="container">
-  <h1>Content</h1>
+  <div>
+    <h1>Societal Problem Identification</h1>
+    <p>Young underprivileged students are adversely affected by having to learn online because of a lack of stable high-speed Internet(Anderson) and because they lack the knowledge and support at home needed to adapt when the Internet fails.</p>
+  </div>
 </div>
 <? # Do not change things below this line ?>
 <?php
