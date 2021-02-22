e40524a386603f15fac3ebd0f36d1c00a22ef9fb
dhanbury <dhanb001@odu.edu>
2021-02-22 12:10:28 -0500

    Change bios.php and styles.css to meet comments/requirements in pull request.

--- bios.php
+++ bios.php
@@ -6,79 +6,82 @@ include_once('header.php');
 
 <? # Add custom HTML content below this line ?>
 <!--Bios using CARD feature of Bootstrap. Organized in 2 rows with 3 cards(in cols) per row.-->
-<div class="row">
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810743154252054588/Colton.jpg" alt="Colton_Img">
-      <div class="card-body">
-        <h5 class="card-title">Colton Hurst</h5>
-        <p class="card-text">Colton Hurst is a senior at ODU, completing his Computer Science degree
+<div class="container">
+  <h1>Biographies</h1>
+  <article class="row">
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810743154252054588/Colton.jpg" alt="Colton_Img">
+        <section class="card-body">
+          <h5 class="card-title">Colton Hurst</h5>
+          <p class="card-text">Colton Hurst is a senior at ODU, completing his Computer Science degree
               while working full time as a software engineer at PDI Software. In his scarce
               free time he enjoys Rust programming, working out, and cooking tasty food.</p>
+        </section>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810702556606496768/IMG-4434_square.jpg" alt="Dalton_Img">
-      <div class="card-body">
-        <h5 class="card-title">Dalton Hanbury</h5>
-        <p class="card-text">Dalton Hanbury is a Third year student at ODU studying
-          for a Bachelor's in Computer Science. He's currently working part time for
-          Nauticus in downtown Norfolk as a Museum Educator. When he's not doing
-          classwork or working at Nauticus, he's usually playing airsoft with his team
-          or building models.</p>
+    </article>
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810702556606496768/IMG-4434_square.jpg" alt="Dalton_Img">
+        <section class="card-body">
+          <h5 class="card-title">Dalton Hanbury</h5>
+          <p class="card-text">Dalton Hanbury is a Third year student at ODU studying
+            for a Bachelor's in Computer Science. He's currently working part time for
+            Nauticus in downtown Norfolk as a Museum Educator. When he's not doing
+            classwork or working at Nauticus, he's usually playing airsoft with his team
+            or building models.</p>
+        </section>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810730405702795274/Screenshot_20210214-2328373.png" alt="Grant_Img">
-      <div class="card-body">
-        <h5 class="card-title">Grant Ralls</h5>
-        <p class="card-text">Grant Ralls is a Computer Science student at Old Dominion University and
-          Software Engineer for Ferguson Enterprises. Outside of technology he enjoys
-          reading and listening to music.</p>
+    </article>
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810730405702795274/Screenshot_20210214-2328373.png" alt="Grant_Img">
+        <section class="card-body">
+          <h5 class="card-title">Grant Ralls</h5>
+          <p class="card-text">Grant Ralls is a Computer Science student at Old Dominion University and
+            Software Engineer for Ferguson Enterprises. Outside of technology he enjoys
+            reading and listening to music.</p>
+        </section>
       </div>
-    </div>
-  </div>
-</div>
-<div class="row">
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810720680604991538/GregPFP.png" alt="Greg_Img">
-      <div class="card-body">
-        <h5 class="card-title">Greg Hubbard</h5>
-        <p class="card-text">Gregory Hubbard is a 4th year computer science student
-          with a passion for learning. Currently, he works as an undergraduate research
-          assistant in developing computer vision based behavioral imaging for
-          clinical measures of autism spectrum disorder.  His future goals include
-          bridging the diverse applications of computer science to medicine.</p>
+    </article>
+  </article>
+  <article class="row">
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810720680604991538/GregPFP.png" alt="Greg_Img">
+        <section class="card-body">
+          <h5 class="card-title">Greg Hubbard</h5>
+          <p class="card-text">Gregory Hubbard is a 4th year computer science student
+            with a passion for learning. Currently, he works as an undergraduate research
+            assistant in developing computer vision based behavioral imaging for
+            clinical measures of autism spectrum disorder.  His future goals include
+            bridging the diverse applications of computer science to medicine.</p>
+        </section>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810583547353366558/Treehouse_FSJS_Profile_crop.jpg" alt="Randy_Img">
-      <div class="card-body">
-        <h5 class="card-title">Randy Layne</h5>
-        <p class="card-text">Randy Layne is a linked Bachelor/Master Computer Science
-          student at ODU and a full time Ruby on Rails developer and Snowflake SQL
-          data engineer for Treehouse. Outside of studying and working he enjoys
-          being a single dad of a wonderful 12 year old lady.</p>
+    </article>
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810583547353366558/Treehouse_FSJS_Profile_crop.jpg" alt="Randy_Img">
+        <section class="card-body">
+          <h5 class="card-title">Randy Layne</h5>
+          <p class="card-text">Randy Layne is a linked Bachelor/Master Computer Science
+            student at ODU and a full time Ruby on Rails developer and Snowflake SQL
+            data engineer for Treehouse. Outside of studying and working he enjoys
+            being a single dad of a wonderful 12 year old lady.</p>
+        </section>
       </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card border-dark mb-3" style="width: 18rem;">
-      <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810890734198587432/travis.jpg" alt="Travis_Img">
-      <div class="card-body">
-        <h5 class="card-title">Travis Bennett</h5>
-        <p class="card-text">Travis Bennett is a computer science senior at ODU. In his free time he
-          enjoys watching sports, and playing games with family.</p>
+    </article>
+    <article class="col">
+      <div class="card border-dark mb-3 card-size bg-transparent-25">
+        <img class="card-img-top" src="https://cdn.discordapp.com/attachments/808482595339894794/810890734198587432/travis.jpg" alt="Travis_Img">
+        <section class="card-body">
+          <h5 class="card-title">Travis Bennett</h5>
+          <p class="card-text">Travis Bennett is a computer science senior at ODU. In his free time he
+            enjoys watching sports, and playing games with family.</p>
+        </section>
       </div>
-    </div>
-  </div>
+    </article>
+  </article>
 </div>
 
 <? # Do not change things below this line ?>
