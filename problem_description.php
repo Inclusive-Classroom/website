520d95ee95bfbefb375d939669c137036df2d7e8
Grant Ralls <grall001@odu.edu>
2021-02-22 18:11:33 -0500

    removing Anderson

--- problem_description.php
+++ problem_description.php
@@ -8,7 +8,7 @@ include_once('header.php');
 <div class="container">
   <div>
     <h1>Societal Problem Identification</h1>
-    <p>Young underprivileged students are adversely affected by having to learn online because of a lack of stable high-speed Internet(Anderson) and because they lack the knowledge and support at home needed to adapt when the Internet fails.</p>
+    <p>Young underprivileged students are adversely affected by having to learn online because of a lack of stable high-speed Internet and because they lack the knowledge and support at home needed to adapt when the Internet fails.</p>
   </div>
   <div>
     <h1>Problem Characteristics</h1>
