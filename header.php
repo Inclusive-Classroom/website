1007e63e0195369db33dd075efed0ab4120d2a9b
dhanbury <dhanb001@odu.edu>
2021-02-23 14:58:48 -0500

    Add shadow to header and jumbotron, and bottom margin to presentations

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="bg-golden-yellow">
-<nav class="navbar navbar-expand-lg navbar-dark bg-gold-foil">
+<nav class="navbar navbar-expand-lg navbar-dark bg-gold-foil shadow-sm">
     <a class="navbar-brand" href="#">Gold Team</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -21,13 +21,10 @@
                 <a class="nav-link <?= ($PageTitle==="Bios") ? "active\" href='./bios.php'>Bios <span class='sr-only'>(current)</span>" : "\" href='./bios.php'>Bios" ?></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?= ($PageTitle==="Reference") ? "active\" href='./reference.php'>References <span class='sr-only'>(current)</span>" : "\" href='./reference.php'>References" ?></a>
+                <a class="nav-link <?= ($PageTitle==="Reference") ? "active\" href='./reference.php'>Reference <span class='sr-only'>(current)</span>" : "\" href='./reference.php'>Reference" ?></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link <?= ($PageTitle==="Presentations") ? "active\" href='./presentations.php'>Presentations <span class='sr-only'>(current)</span>" : "\" href='./presentations.php'>Presentations" ?></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= ($PageTitle==="Problem Description") ? "active\" href='./problem_description.php'>Problem Description <span class='sr-only'>(current)</span>" : "\" href='./problem_description.php'>Problem Description" ?></a>
             </li>
         </ul>
     </div>
