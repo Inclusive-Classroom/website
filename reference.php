4b8caabee85bf00d4f4681c16956e4f7c4f33e51
dhanbury <dhanb001@odu.edu>
2021-03-01 15:28:48 -0500

    Update reference.php. Use bootstrap list groups with Hrefs.

--- reference.php
+++ reference.php
@@ -7,52 +7,19 @@ include_once('header.php');
 <? # Add custom HTML content below this line ?>
 <div class="container">
     <h1>References</h1>
-
-    <ol>
-        <li id="ref-1">
-            <a href="http://www.lcsedu.net/departments/information-technology/chromebook-support">“Chromebook Support.” <i>LCS</i>, www.lcsedu.net/departments/information-technology/chromebook-support.</a>
-        </li>
-        <br> <!--Use br to maintain proper spacing between list items. Spacing of <p> was lost when replaced with <a>.-->
-        <li id="ref-2">
-            <a href="http://support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444">“Classroom FAQ - Classroom Help.” <i>Google</i>, Google, support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444.</a>
-        </li>
-        <br>
-        <li id="ref-3">
-            <a href="http://www.cs.odu.edu/~cpi/old/410/orangf20/">Del Razo, Gabriel, et al. “LittleLEARNERS.” <i>Team Orange</i>, www.cs.odu.edu/~cpi/old/410/orangf20/.</a>
-        </li>
-        <br>
-        <li id="ref-4">
-            <a href="http://www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html">Raphael, JR. “Android Apps for Chromebooks: The Essentials.” <i>Computerworld</i>, Computerworld, 19 Feb. 2019, www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html.</a>
-        </li>
-        <br>
-        <li id="ref-5">
-            <a href="http://www.vbschools.com/news/archived_news/2019/chromebooks">VBCPS. “VBCPS Adds 19,000 Chromebooks to Achieve 1:1.” <i>Virginia Beach City Public Schools</i>, www.vbschools.com/news/archived_news/2019/chromebooks.</a>
-        </li>
-        <br>
-        <li id="ref-6">
-            <a href="http://www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/">VDH. “COVID 19 Cases In Virginia.” <i>Virginia Department of Health</i>, www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/.</a>
-        </li>
-        <br>
-        <li id="ref-7">
-            <a href="http://www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/">Vogels, Emily A. “59% Of U.S. Parents with Lower Incomes Say Their Child May Face Digital Obstacles in Schoolwork.” <i>Pew Research Center</i>, Pew Research Center, 10 Sept. 2020, www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/.</a>
-        </li>
-        <br>
-        <li id="ref-8">
-            <a href="http://www.spring.io/web-applications">“Web Applications with Spring.” <i>Spring</i>, spring.io/web-applications.</a>
-        </li>
-        <br>
-        <li id="ref-9">
-            <a href="http://www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/">Anderson, Monica, and Andrew Perrin. “Nearly One-in-Five Teens Can't Always Finish Their Homework Because of the Digital Divide.” Pew Research Center, Pew Research Center, 30 May 2020, www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/.</a>
-        </li>
-        <br>
-        <li id="ref-10">
-            <a href="http://www.web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html">ICTP Science Dissemination Unit, ICTP Science Dissemination. “ICTP-SDU Home Page.” ICTP-SDU: about PingER, web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html.</a>
-        </li>
-        <br>
-        <li id="ref-11">
-            <a href="http://support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux">“System Requirements for Windows, MacOS, and Linux.” Zoom Help Center, support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux.</a>
-        </li>
-    </ol>
+    <div class="list-group list-group-flush">
+        <a id="ref-1" href="http://www.lcsedu.net/departments/information-technology/chromebook-support" class="list-group-item list-group-item-action" target="_blank">1. “Chromebook Support.” <i>LCS</i>, www.lcsedu.net/departments/information-technology/chromebook-support.</a>
+        <a id="ref-2" href="http://support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444" class="list-group-item list-group-item-action" target="_blank">2. “Classroom FAQ - Classroom Help.” <i>Google</i>, Google, support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444.</a>
+        <a id="ref-3" href="http://www.cs.odu.edu/~cpi/old/410/orangf20/" class="list-group-item list-group-item-action" target="_blank">3. Del Razo, Gabriel, et al. “LittleLEARNERS.” <i>Team Orange</i>, www.cs.odu.edu/~cpi/old/410/orangf20/.</a>
+        <a id="ref-4" href="http://www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html" class="list-group-item list-group-item-action" target="_blank">4. Raphael, JR. “Android Apps for Chromebooks: The Essentials.” <i>Computerworld</i>, Computerworld, 19 Feb. 2019, www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html.</a>
+        <a id="ref-5" href="http://www.vbschools.com/news/archived_news/2019/chromebooks" class="list-group-item list-group-item-action" target="_blank">5. VBCPS. “VBCPS Adds 19,000 Chromebooks to Achieve 1:1.” <i>Virginia Beach City Public Schools</i>, www.vbschools.com/news/archived_news/2019/chromebooks.</a>
+        <a id="ref-6" href="http://www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/" class="list-group-item list-group-item-action" target="_blank">6. VDH. “COVID 19 Cases In Virginia.” <i>Virginia Department of Health</i>, www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/.</a>
+        <a id="ref-7" href="http://www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/" class="list-group-item list-group-item-action" target="_blank">7. Vogels, Emily A. “59% Of U.S. Parents with Lower Incomes Say Their Child May Face Digital Obstacles in Schoolwork.” <i>Pew Research Center</i>, Pew Research Center, 10 Sept. 2020, www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/.</a>
+        <a id="ref-8" href="http://www.spring.io/web-applications" class="list-group-item list-group-item-action" target="_blank">8. “Web Applications with Spring.” <i>Spring</i>, spring.io/web-applications.</a>
+        <a id="ref-9" href="http://www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/" class="list-group-item list-group-item-action" target="_blank">9. Anderson, Monica, and Andrew Perrin. “Nearly One-in-Five Teens Can't Always Finish Their Homework Because of the Digital Divide.” Pew Research Center, Pew Research Center, 30 May 2020, www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/.</a>
+        <a id="ref-10" href="http://www.web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html" class="list-group-item list-group-item-action" target="_blank">10. ICTP Science Dissemination Unit, ICTP Science Dissemination. “ICTP-SDU Home Page.” ICTP-SDU: about PingER, web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html.</a>
+        <a id="ref-11" href="http://support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux" class="list-group-item list-group-item-action" target="_blank">11. “System Requirements for Windows, MacOS, and Linux.” Zoom Help Center, support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux.</a>
+    </div>
 </div>
 <? # Do not change things below this line ?>
 <?php
