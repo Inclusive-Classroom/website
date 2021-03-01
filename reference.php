c82d51ee231b2f5d6db4f143b9a74a512ed3b626
dhanbury <dhanb001@odu.edu>
2021-03-01 13:34:10 -0500

    Update references page, list items link to webpages in reference via href and anchor

--- reference.php
+++ reference.php
@@ -9,29 +9,48 @@ include_once('header.php');
     <h1>References</h1>
 
     <ol>
-        <li>
-            <p>“Chromebook Support.” <i>LCS</i>, www.lcsedu.net/departments/information-technology/chromebook-support.</p>
+        <li id="ref-1">
+            <a href="http://www.lcsedu.net/departments/information-technology/chromebook-support">“Chromebook Support.” <i>LCS</i>, www.lcsedu.net/departments/information-technology/chromebook-support.</a>
         </li>
-        <li>
-            <p>“Classroom FAQ - Classroom Help.” <i>Google</i>, Google, support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444.</p>
+        <br> <!--Use br to maintain proper spacing between list items. Spacing of <p> was lost when replaced with <a>.-->
+        <li id="ref-2">
+            <a href="http://support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444">“Classroom FAQ - Classroom Help.” <i>Google</i>, Google, support.google.com/edu/classroom/answer/6025224?hl=en&ref_topic=7175444.</a>
         </li>
-        <li>
-            <p>Del Razo, Gabriel, et al. “LittleLEARNERS.” <i>Team Orange</i>, www.cs.odu.edu/~cpi/old/410/orangf20/.</p>
+        <br>
+        <li id="ref-3">
+            <a href="http://www.cs.odu.edu/~cpi/old/410/orangf20/">Del Razo, Gabriel, et al. “LittleLEARNERS.” <i>Team Orange</i>, www.cs.odu.edu/~cpi/old/410/orangf20/.</a>
         </li>
-        <li>
-            <p>Raphael, JR. “Android Apps for Chromebooks: The Essentials.” <i>Computerworld</i>, Computerworld, 19 Feb. 2019, www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html.</p>
+        <br>
+        <li id="ref-4">
+            <a href="http://www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html">Raphael, JR. “Android Apps for Chromebooks: The Essentials.” <i>Computerworld</i>, Computerworld, 19 Feb. 2019, www.computerworld.com/article/3234533/android-apps-for-chromebooks-the-essentials.html.</a>
         </li>
-        <li>
-            <p>VBCPS. “VBCPS Adds 19,000 Chromebooks to Achieve 1:1.” <i>Virginia Beach City Public Schools</i>, www.vbschools.com/news/archived_news/2019/chromebooks.</p>
+        <br>
+        <li id="ref-5">
+            <a href="http://www.vbschools.com/news/archived_news/2019/chromebooks">VBCPS. “VBCPS Adds 19,000 Chromebooks to Achieve 1:1.” <i>Virginia Beach City Public Schools</i>, www.vbschools.com/news/archived_news/2019/chromebooks.</a>
         </li>
-        <li>
-            <p>VDH. “COVID 19 Cases In Virginia.” <i>Virginia Department of Health</i>, www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/.</p>
+        <br>
+        <li id="ref-6">
+            <a href="http://www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/">VDH. “COVID 19 Cases In Virginia.” <i>Virginia Department of Health</i>, www.vdh.virginia.gov/coronavirus/coronavirus/covid-19-in-virginia-cases/.</a>
         </li>
-        <li>
-            <p>Vogels, Emily A. “59% Of U.S. Parents with Lower Incomes Say Their Child May Face Digital Obstacles in Schoolwork.” <i>Pew Research Center</i>, Pew Research Center, 10 Sept. 2020, www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/.</p>
+        <br>
+        <li id="ref-7">
+            <a href="http://www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/">Vogels, Emily A. “59% Of U.S. Parents with Lower Incomes Say Their Child May Face Digital Obstacles in Schoolwork.” <i>Pew Research Center</i>, Pew Research Center, 10 Sept. 2020, www.pewresearch.org/fact-tank/2020/09/10/59-of-u-s-parents-with-lower-incomes-say-their-child-may-face-digital-obstacles-in-schoolwork/.</a>
         </li>
-        <li>
-            <p>“Web Applications with Spring.” <i>Spring</i>, spring.io/web-applications.</p>
+        <br>
+        <li id="ref-8">
+            <a href="http://www.spring.io/web-applications">“Web Applications with Spring.” <i>Spring</i>, spring.io/web-applications.</a>
+        </li>
+        <br>
+        <li id="ref-9">
+            <a href="http://www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/">Anderson, Monica, and Andrew Perrin. “Nearly One-in-Five Teens Can't Always Finish Their Homework Because of the Digital Divide.” Pew Research Center, Pew Research Center, 30 May 2020, www.pewresearch.org/fact-tank/2018/10/26/nearly-one-in-five-teens-cant-always-finish-their-homework-because-of-the-digital-divide/.</a>
+        </li>
+        <br>
+        <li id="ref-10">
+            <a href="http://www.web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html">ICTP Science Dissemination Unit, ICTP Science Dissemination. “ICTP-SDU Home Page.” ICTP-SDU: about PingER, web.archive.org/web/20131010010244/sdu.ictp.it/pinger/pinger.html.</a>
+        </li>
+        <br>
+        <li id="ref-11">
+            <a href="http://support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux">“System Requirements for Windows, MacOS, and Linux.” Zoom Help Center, support.zoom.us/hc/en-us/articles/201362023-System-Requirements-for-PC-Mac-and-Linux.</a>
         </li>
     </ol>
 </div>
