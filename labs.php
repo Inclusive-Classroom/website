6eda837f12a6cb08637009b75695f95b37004eb6
rlayne <rlayn001@odu.edu>
2021-04-14 08:16:37 -0400

    Fix typos in labs.php. Add updated lab pdf.

--- labs.php
+++ labs.php
@@ -7,10 +7,10 @@ include_once('header.php');
 <? # Add custom HTML content below this line ?>
 
 <div class="container mb-5">
-  <h1>Lab 1</h1>
+  <h1>Labs</h1>
   <article class="text-center">
-    <h2 class="mt-2">Idea Presentation</h2>
-    <iframe src="assets/lab-1-outline.pdf" frameborder="0" allowfullscreen width="640" height="800"></iframe>
+    <h2 class="mt-2">Lab 1</h2>
+    <iframe src="assets/lab-1-outline.pdf" frameborder="0" marginwidth="0" allowfullscreen width="640" height="800"></iframe>
   </article>
 </div>
 
