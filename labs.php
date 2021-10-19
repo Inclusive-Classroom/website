f8b371d7349a029c784237c5f3f41982a68682f2
adavie <adavi025@odu.edu>
2021-10-19 14:52:59 -0400

    Add Lab 1 V1 individual reports and placeholders for Lab 2 V1

--- labs.php
+++ labs.php
@@ -16,6 +16,41 @@ include_once('header.php');
     <h2 class="mt-2">Lab 1 outline v2.0</h2>
     <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ9OQ83mFTs3ls7hLZOz7lZMyGEuxUQvJvolhAgzAubcHiTuJqOl3JfHvCmF8x0FXQcGj5BszT9fYXp/pub?embedded=true" frameborder="0" marginwidth="auto" allowfullscreen width="75%"  height="800"></iframe>
   </article>
+
+  <!--------Lab 1 Individual Reports-------->
+  <article class="text-center">
+    <h2 class="mt-2">Lab 1 Individual Reports</h2>
+    <h3 class="mt-2">Product Description Version 1</h3>
+    <ul>
+        <li>
+        <a href="/assets/Bennett_Lab1_V1.pdf">Travis Bennett</a>
+        </li>
+        <li>
+        <a href="/assets/Hanbury_Lab1_V1.pdf">Dalton Hanbury</a>
+        </li>
+        <li>
+        <a href="/assets/Hubbard_Lab1_V1.pdf">Greg Hubbard</a>
+        </li>
+        <li>
+        <a href="/assets/Hurst_Lab1_V1.pdf">Colton Hurst</a>
+        </li>
+        <li>
+        <a href="/assets/Layne_Lab1_V1.pdf">Randy Layne</a>
+        </li>
+    </ul>
+  </article>
+
+<!--------Lab 2 Group Report-------->
+  <article class="text-center">
+    <h2 class="mt-2">Lab 2 Group Report</h2>
+    <h3 class="mt-2">Requirements</h3>
+  </article>
+
+<!--------Lab 2 Individual Reports-------->
+  <article class="text-center">
+    <h2 class="mt-2">Lab 2 Individual Reports</h2>
+    <h3 class="mt-2">Product Specification</h3>
+  </article>
 </div>
 
 <? # Do not change things below this line ?>
